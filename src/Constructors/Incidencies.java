2e062be00676defe941a7fd5b9bbfb7ff901c34e
Evaldas <evaldas_casas@hotmail.com>
2019-01-11 17:25:30 +0100

    Formatacion Clase Incidencia
    AY LMAO

--- src/Constructors/Incidencies.java
+++ src/Constructors/Incidencies.java
@@ -1,13 +1,14 @@
-
 package Constructors;
 
 import Public.Public;
 
 /**
  * Classe amb el constructor,setters i getters dels objectes tipus incidencies
+ *
  * @author ivan
  */
 public class Incidencies {
+
     private String titolIncidencia;                                             //titol de la incidencia
     private String descripcioIncidencia;                                        //descripcio de l'incidencia
     private String lloc;                                                        //zona on ocorre l'incidencia
@@ -19,100 +20,101 @@ public class Incidencies {
     /**
      * Constructor de Incidencies sense pas de parametres
      */
-    public Incidencies (){
+    public Incidencies() {
         ++Public.comptador_incidencies_id;
-        titolIncidencia ="";
-        descripcioIncidencia="";
-        lloc="";
-        date="";
-        estat="";
-        user="";
+        titolIncidencia = "";
+        descripcioIncidencia = "";
+        lloc = "";
+        date = "";
+        estat = "";
+        user = "";
         id = "ID_" + Public.comptador_incidencies_id;
     }
-    
+
     /**
      * Constructor de Incidencies amb pas de parametres
+     *
      * @param n1
      * @param n2
      * @param n3
      * @param n4
-     * @param n5 
+     * @param n5
      */
-     public Incidencies (String n1, String n2, String n3, String n4, String n5){
+    public Incidencies(String n1, String n2, String n3, String n4, String n5) {
         ++Public.comptador_incidencies_id;
         titolIncidencia = n1;
-        descripcioIncidencia= n2;
-        lloc= n3;
-        user= n4;
-        date= n5;
+        descripcioIncidencia = n2;
+        lloc = n3;
+        user = n4;
+        date = n5;
         id = "ID_" + Public.comptador_incidencies_id;
     }
-    
-    public String gettitolIncidencia(){
+
+    public String gettitolIncidencia() {
         return titolIncidencia;
     }
-    
-    public String getdescripcioIncidencia(){
+
+    public String getdescripcioIncidencia() {
         return descripcioIncidencia;
     }
-    
-    public String getlloc(){
+
+    public String getlloc() {
         return lloc;
     }
-    
-    public String getdate(){
+
+    public String getdate() {
         return date;
     }
-    
-    public String getestat(){
+
+    public String getestat() {
         return estat;
     }
-    
-     public String getuser(){
+
+    public String getuser() {
         return user;
     }
-     
-     public String getid(){
-         return id;
-     }
-     
-    public void settitolIncidencia(String t){
-       this.titolIncidencia = t;
+
+    public String getid() {
+        return id;
     }
-    
-    public void setdescripcioIncidencia(String i){
+
+    public void settitolIncidencia(String t) {
+        this.titolIncidencia = t;
+    }
+
+    public void setdescripcioIncidencia(String i) {
         this.descripcioIncidencia = i;
     }
-    
-    public void setlloc (String l){
+
+    public void setlloc(String l) {
         this.lloc = l;
     }
-    
-    public void setdate (String d){
+
+    public void setdate(String d) {
         this.date = d;
     }
-        public void setuser (String u){
+
+    public void setuser(String u) {
         this.user = u;
     }
-    
-    public void setestat (String e){
+
+    public void setestat(String e) {
         this.estat = e;
     }
-    
-    public void setid (String p){
+
+    public void setid(String p) {
         this.id = p;
     }
 
-
-
-
     /**
      * Metode per a obtindre totes les dades d'una incidencia
-     * @return String "titolIncidencia+" "+descripcioIncidencia+" "+lloc+" "+date+" "+user"
+     *
+     * @return String "titolIncidencia+" "+descripcioIncidencia+" "+lloc+"
+     * "+date+" "+user"
      */
     @Override
-    public String toString(){
-        return titolIncidencia+" "+descripcioIncidencia+" "+lloc+" "+date+" "+user;
-    }    
+    public String toString() {
+        return titolIncidencia + " " + descripcioIncidencia + " " + lloc + " " + date + " " + user;
+    }
 
 }
